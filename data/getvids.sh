67a2519089dbc8a1f3c63313a98d83997d1a27ca
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-11-12 15:40:41 -0500

    Added data dir

--- data/getvids.sh
+++ data/getvids.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# Make sure user has youtube-dl (http://rg3.github.io/youtube-dl) for downloading videos
+# Dependency: youtube-dl (http://rg3.github.io/youtube-dl) for downloading videos
 # Run 'pip install youtube-dl --user' to install it
+
 command -v youtube-dl >/dev/null 2>&1 || { echo >&2 "youtube-dl is not installed, please install it.  Aborting."; exit 1; }
-rm -rf data && mkdir data
-cd data
 youtube-dl --no-playlist -a ../videolinks.txt --format mp4
-ls | cat -n | while read n f; do mv "$f" "$n.mp4"; done 
+ls *.mp4 | cat -n | while read n f; do mv "$f" "$n.mp4"; done 
