80dcfa24a1cf3a65dc94d443e8a4c68db0701681
Taha Azzaoui <taha@tahaazzaoui.com>
2018-11-20 21:54:01 -0500

    Added video attribute logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
-#define THRESH_VAL 200 
+#define THRESH_VAL 200
+#define DEBUG 1
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -13,17 +14,33 @@ int main(int argc, char* argv[]) {
 
   cv::Mat frame, thresh;
   cv::VideoCapture cap(argv[1]);
-  
+  size_t fps, frame_count, width, height;
+
   if (!cap.isOpened()) {
     std::cerr << "Failed to play file " << argv[1] << std::endl;
+    std::cerr << cv::getBuildInformation() << std::endl;
     return -2;
   }
 
-  while (cvWaitKey(10) != 27 && cap.read(frame)){
-      cv::cvtColor(frame, thresh, CV_BGR2GRAY);
-    //cv::threshold(thresh, thresh, THRESH_VAL, 0xff, CV_THRESH_BINARY); 
-      cv::Canny(thresh, thresh, 100, 200);
-      cv::imshow("window", thresh);
+  fps = cap.get(CV_CAP_PROP_FPS);
+  frame_count = cap.get(CV_CAP_PROP_FRAME_COUNT);
+  width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+  height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+
+  if (DEBUG) {
+    std::cout << "Read video file: " << argv[1] << std::endl;
+    std::cout << "FPS = " << fps << std::endl;
+    std::cout << "Frame Count = " << frame_count << std::endl;
+    std::cout << "Frame Width = " << width << std::endl;
+    std::cout << "Frame Height = " << height << std::endl;
   }
+
+  while (cvWaitKey(10) != 27 && cap.read(frame)) {
+    cv::cvtColor(frame, thresh, CV_BGR2GRAY);
+    // cv::threshold(thresh, thresh, THRESH_VAL, 0xff, CV_THRESH_BINARY);
+    cv::Canny(thresh, thresh, 100, 200);
+    cv::imshow("window", thresh);
+  }
+
   return 0;
 }
