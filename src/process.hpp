0ac1c3824f3ba1ed52178a8a29bb8c27b2f25830
Taha Azzaoui <taha@tahaazzaoui.com>
2018-11-22 01:31:26 -0500

    Updated processing definitions

--- src/process.hpp
+++ src/process.hpp
@@ -4,7 +4,10 @@
 #include <vector>
 #include <opencv2/opencv.hpp>
 
-typedef unsigned int uint;
+struct Contours{
+    std::vector<std::vector<cv::Point>> pts;
+    std::vector<std::array<int, 2>> centroids;
+};
 
 class Process {
  private:
@@ -13,8 +16,7 @@ class Process {
  public:
   Process(const cv::Mat&);
   void filter_frame(cv::Mat&);
-  int get_contours(const uint, const uint,
-                   std::vector<std::vector<cv::Point>>&);
+  Contours find_contours(const cv::Mat&, cv::Mat&);
 };
 
 #endif /* PROCESS_HPP */
